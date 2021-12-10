b889314bb39ef416d1726588c93158730f795050
Tega Okorare <epicboy006@gmail.com>
2021-12-10 08:50:49 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+        <Head>
+      <title>Ninja List | About</title>
+      <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid quod dolorum delectus non quaerat minima commodi provident porro magnam, maiores ea optio, consequuntur cupiditate reprehenderit at odit, eligendi rerum!</p>
+        </div>
+        </>
+    );
+}
+ 
+export default About;
